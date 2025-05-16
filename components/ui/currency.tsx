e7ab531e06679805547cb4eaa04c14678add629a
jainil shah <97247891+jainilshah03@users.noreply.github.com>
2025-05-16 14:01:40 +0530

    Removed boolean type

--- components/ui/currency.tsx
+++ components/ui/currency.tsx
@@ -4,7 +4,7 @@ import { formatter } from "@/lib/utils";
 import { useEffect, useState } from "react";
 
 interface CurrencyProps {
-    value?: string | number | boolean;
+    value?: string | number;
 }
 
 const Currency: React.FC<CurrencyProps> = ({
